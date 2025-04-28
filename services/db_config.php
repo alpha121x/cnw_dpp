a1aa9dd98e8200282e165df7580d021c3ecee227
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-04-28 15:41:13 +0500

    Update db_config.php

--- services/db_config.php
+++ services/db_config.php
@@ -4,19 +4,17 @@ $host = 'localhost';
 $dbname = 'db_cnw_dpp'; // Your database name
 $user = 'postgres'; // Your PostgreSQL username
 $password = '1234'; // Your PostgreSQL password
-$port = '5433'; // Default PostgreSQL port
+$port = '5432'; // Default PostgreSQL port
 
 // DSN (Data Source Name) for PostgreSQL
-$dsn = "pgsql:host=$host;dbname=$dbname;port=$port";
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
 
 try {
     // Create a PDO instance
     $pdo = new PDO($dsn, $user, $password);
-
+    
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // echo "Connection successful!";
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
