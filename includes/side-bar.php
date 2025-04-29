37833184505712ee856e07fa5f4715b559d8980a
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-04-29 15:29:40 +0500

    Update side-bar.php

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -15,16 +15,17 @@
       <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
     <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+    <li>
+            <a href="create_work_orders.php">
+                <i class="bi bi-circle"></i><span>Create Work Orders</span>
+            </a>
+        </li>
         <li>
             <a href="work_orders_issuance.php">
                 <i class="bi bi-circle"></i><span>Work Orders Issuance</span>
             </a>
         </li>
-        <li>
-            <a href="create_work_orders.php">
-                <i class="bi bi-circle"></i><span>Create Work Orders</span>
-            </a>
-        </li>
+       
     </ul>
   </li><!-- End Components Nav -->
 
