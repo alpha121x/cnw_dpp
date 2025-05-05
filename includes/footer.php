ecf7b4b35cab6faaf33663249c979eb2f901ebe1
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-05-05 15:45:21 +0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright C&W - DPP. All Rights Reserved Designed by <strong>The Urban Unit</strong>
+      &copy; Copyright <strong>C&W - DPP.</strong> All Rights Reserved Designed by <strong>The Urban Unit</strong>
     </div>
   </footer><!-- End Footer -->
