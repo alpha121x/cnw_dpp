64ac5e7c7e68288e603e5f32c559468549ec0138
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-05-13 14:54:41 +0500

    Update footer-src-files.php

--- includes/footer-src-files.php
+++ includes/footer-src-files.php
@@ -1,13 +1,7 @@
 <!-- Vendor JS Files -->
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-<!-- <script src="assets/vendor/apexcharts/apexcharts.min.js"></script> -->
 <!-- Use Bootstrap 5 CDN to ensure it loads -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<!-- <script src="assets/vendor/chart.js/chart.umd.js"></script>
-<script src="assets/vendor/echarts/echarts.min.js"></script>
-<script src="assets/vendor/quill/quill.js"></script>
-<script src="assets/vendor/tinymce/tinymce.min.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script> -->
 <!-- DataTables JS with Bootstrap Integration -->
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
